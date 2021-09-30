31df1ba0f8bdf58d2adb2fa7a9322641efdd7bc0
Nikolay Zhuravlev <znick@znick.ru>
2021-10-01 00:27:24 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-NAME=$1
-RO_DIR=$2
+NAME=$1 # название задача
+RO_DIR=$2 # read-only каталог с задачей
 RW_DIR=/task_rw
 PY_FILES=$RW_DIR/*.py
 PY_FILES_RO=$RO_DIR/*.py
 PY_FILES_N=$(ls -1 $RO_DIR/*.py | wc -l)
 
-python3 $PY_FILES_RO
+python3 $PY_FILES_RO # запускаем проверку
+
 
